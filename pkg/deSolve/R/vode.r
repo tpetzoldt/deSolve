c98d9067839b806180d19b2fa31c9be9fc8387ee
karlines <karlines@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-04-09 11:22:11 +0000

    if model not a DLL, initpar <- NULL

--- pkg/deSolve/R/vode.r
+++ pkg/deSolve/R/vode.r
@@ -136,6 +136,7 @@ vode          <- function(y, times, func, parms, rtol=1e-6, atol=1e-8,
          Nmtot <- c(outnames,(length(outnames)+1):nout)
 
     }  else {
+        initpar <- NULL # parameter initialisation not needed if function is not a DLL    
         rho <- environment(func)
       # func and jac are overruled, either including ynames, or not
       # This allows to pass the "..." arguments and the parameters
