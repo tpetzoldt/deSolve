ef7ab7d1acc3f13dcc65421c9721b0da951e9eba
karlines <karlines@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-04-14 05:43:41 +0000

    Function ode.1D now also works with compiled code. Adapted its help file. Added a biological example of ode.1D (lotka-Volterra pred-prey diffusing on a 2-D surface). Made small changes to help files of lsodes, ode.band.

--- pkg/deSolve/R/ode.r
+++ pkg/deSolve/R/ode.r
@@ -2,7 +2,7 @@
 ### ode.1D, ode.band -- solves banded ordinary differential equation systems 
 ### ode.1D is designed for solving multi-component 1-D reaction-transport models 
 ### ode.band is designed for solving single-component 1-D reaction-transport models
-### they offer the choice between the integrators vode, lsode and lsoda.
+### they offer the choice between the integrators vode, lsode, lsoda, lsodar and lsodes.
 
 
 ode.1D    <- function (y,
@@ -12,6 +12,17 @@ ode.1D    <- function (y,
                        method= "lsode",
                        ...)
 {
+  if (hasArg(jacfunc)) stop ("cannot run ode.1D with jacfunc specified - remove jacfunc from call list")
+  if (is.null(nspec)  ) stop ("cannot run ode.1D: nspec is not specified")
+  N     <- length(y)
+
+  if (is.character(func) || method=="lsodes")
+  {
+    if ( method != "lsodes") warning("ode.1D: R-function specified in a DLL-> integrating with lsodes") 
+    # use lsodes
+    out <- lsodes(y=y,times=times,func=func,...)                    
+ 
+  } else {
   # internal function #
   bmodel <- function (time,state,pars,model,...)
   {
@@ -20,10 +31,7 @@ ode.1D    <- function (y,
   }
 
   if (is.character(func))stop ("cannot run ode.1D with R-function specified in a DLL")
-  if (hasArg(jacfunc)) stop ("cannot run ode.1D with jacfunc specified - remove from call list")
 
-  if (is.null(nspec)  ) stop ("cannot run ode.1D: nspec is not specified")
-  N     <- length(y)
   ii    <- as.vector(t(matrix(ncol=nspec,1:N)))   # from ordering per slice -> per spec
   ij    <- as.vector(t(matrix(nrow=nspec,1:N)))   # from ordering per spec -> per slice
     
@@ -41,7 +49,7 @@ ode.1D    <- function (y,
   else
    stop ("cannot run ode.1D: method should be one of vode, lsoda, lsodar, lsode")   
   out[,(ii+1)] <- out[,2:(N+1)]
-
+  }
   return(out)
 }
 
@@ -66,6 +74,6 @@ if (is.null(method)) method <- "lsode"
   else if (method == "lsodar")
    lsodar(y,times,func,bandup=bandup,banddown=banddown,jactype="bandint",...) 
   else
-   stop ("cannot run ode.band: method should be one of vode, lsoda, lsode")   
+   stop ("cannot run ode.band: method should be one of vode, lsoda, lsodar or lsode")   
   
 }
