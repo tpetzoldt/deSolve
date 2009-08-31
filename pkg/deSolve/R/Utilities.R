8bda06e50a65d0909dba039bd2c2a11db660d990
karlines <karlines@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-08-31 12:32:02 +0000

    Added forcing functions in R code to forcings.Rd; added S3 method hist.deSolve (~ plot.deSolve)

--- pkg/deSolve/R/Utilities.R
+++ pkg/deSolve/R/Utilities.R
@@ -1,4 +1,55 @@
 ### ============================================================================
+### first some common functions
+### ============================================================================
+
+## =============================================================================
+## Set the mfrow parameters and whether to "ask" for opening a new device
+## =============================================================================
+
+setplotpar <- function(nmdots, dots, nv, ask) {
+    if (!any(match(nmdots, c("mfrow", "mfcol"), nomatch = 0))) {
+      nc <- min(ceiling(sqrt(nv)),3)
+      nr <- min(ceiling(nv/nc),3)
+      mfrow <- c(nr, nc)
+    }
+    else if ("mfcol" %in% nmdots)
+        mfrow <- rev(dots$mfcol)
+    else mfrow <- dots$mfrow
+
+    if (! is.null(mfrow)) {
+      mf <- par(mfrow=mfrow)
+    }
+
+   ## interactively wait if there are remaining figures
+    if (is.null(ask))
+      ask <- prod(par("mfrow")) < length(which) && dev.interactive()
+
+    return(ask)
+}
+
+## =============================================================================
+## find a variable
+## =============================================================================
+
+selectvar <- function (which,var) {
+
+    if (!is.numeric(which)) {
+        ln <- length(which)
+        Select <- which(var %in% which)
+        if (length(Select) != ln)
+            stop("not all variables in 'which' are in 'x'")
+    }
+    else {
+        Select <- which + 1  # "Select now refers to the column number
+        if (max(Select) > length(var))
+            stop("index in 'which' too large")
+        if (min(Select) < 1)
+            stop("index in 'which' should be > 0")
+    }
+  return(Select)
+}
+
+### ============================================================================
 ### S3 methods
 ### ============================================================================
 
@@ -10,37 +61,55 @@ print.deSolve <- function(x, ...)
 plot.deSolve <- function (x, which = 1:(ncol(x)-1), ask = NULL, ...) {
     t <- 1     # column with "times"
     var <- colnames(x)
-    if (!is.numeric(which)) {
-        ln <- length(which)
-        Select <- which(var %in% which)
-        if (length(Select) != ln)
-            stop("not all variables in 'which' are in 'x'")
-        which <- Select
-    }
-    else {
-        which <- which + 1  # "which now refers to the column number
-        if (max(which) > ncol(x))
-            stop("index in 'which' too large")
-        if (min(which) < 1)
-            stop("index in 'which' should be > 0")
-    }
+    which <- selectvar(which,var)
+    
     np <- length(which)
 
     dots <- list(...)
     nmdots <- names(dots)
-    if (!any(match(nmdots, c("mfrow", "mfcol"), nomatch = 0))) {
-        nc <- min(ceiling(sqrt(np)), 3)  # assume max 3 x 3 panels
-        nr <- min(ceiling(np/nc), 3)
-        mfrow <- c(nr, nc)
-    }
-    else if ("mfcol" %in% nmdots)
-        mfrow <- rev(dots$mfcol)
-    else
-        mfrow <- dots$mfrow
 
-    if (!is.null(mfrow)) {
-        mf <- par(mfrow = mfrow)
+    # number of figures in a row and 
+    # interactively wait if there are remaining figures
+   
+    ask <- setplotpar(nmdots, dots, np, ask)
+    if (ask) {
+        oask <- devAskNewPage(TRUE)
+        on.exit(devAskNewPage(oask))
     }
+    
+    Main <- is.null(dots$main)
+
+    xxlab <- if (is.null(dots$xlab))  colnames(x)[t]  else dots$xlab
+    yylab <- if (is.null(dots$ylab))  ""              else dots$ylab
+
+    ## allow individual xlab and ylab (vectorized)
+    xxlab <- rep(xxlab, length.out = np)
+    yylab <- rep(yylab, length.out = np)
+
+    for (i in which) {
+        if (Main)
+            dots$main <- colnames(x)[i]
+            dots$xlab <- xxlab[i-1]
+            dots$ylab <- yylab[i-1]
+        do.call("plot", c(alist(x[, t], x[, i]), dots))
+    }
+}
+
+### ============================================================================
+
+hist.deSolve <- function (x, which = 1:(ncol(x)-1), ask = NULL, ...) {
+    t     <- 1     # column with "times"
+    var   <- colnames(x)
+    which <- selectvar(which,var)
+
+    np <- length(which)
+
+    dots   <- list(...)
+    nmdots <- names(dots)
+
+    ## Set par mfrow and ask.
+    ask <- setplotpar(nmdots, dots, np, ask)
+
     ## interactively wait if there are remaining figures
     if (is.null(ask))
         ask <- prod(par("mfrow")) < length(which) && dev.interactive()
@@ -52,6 +121,7 @@ plot.deSolve <- function (x, which = 1:(ncol(x)-1), ask = NULL, ...) {
 
     xxlab <- if (is.null(dots$xlab))  colnames(x)[t]  else dots$xlab
     yylab <- if (is.null(dots$ylab))  ""              else dots$ylab
+
     ## allow individual xlab and ylab (vectorized)
     xxlab <- rep(xxlab, length.out = np)
     yylab <- rep(yylab, length.out = np)
@@ -61,6 +131,6 @@ plot.deSolve <- function (x, which = 1:(ncol(x)-1), ask = NULL, ...) {
             dots$main <- colnames(x)[i]
             dots$xlab <- xxlab[i-1]
             dots$ylab <- yylab[i-1]
-        do.call("plot", c(alist(x[, t], x[, i]), dots))
+        do.call("hist", c(alist(x[, i]), dots))
     }
 }
