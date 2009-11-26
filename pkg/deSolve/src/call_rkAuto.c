2bb6c7566644350d2b0d0b7c4540fef4084c193b
petzoldt <petzoldt@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-11-26 12:37:58 +0000

    call_rkAuto split into two functions; allows to suppress interpolation (then a little bit slower); prerequisite for handling events at discrete time points. Note: experimental!

--- pkg/deSolve/src/call_rkAuto.c
+++ pkg/deSolve/src/call_rkAuto.c
@@ -5,6 +5,8 @@
 
 #include "rk_util.h"
 
+#include "rk_auto.h"
+
 SEXP call_rkAuto(SEXP Xstart, SEXP Times, SEXP Func, SEXP Initfunc,
   SEXP Parms, SEXP Nout, SEXP Rho,
   SEXP Rtol, SEXP Atol, SEXP Tcrit, SEXP Verbose,
@@ -20,15 +22,14 @@ SEXP call_rkAuto(SEXP Xstart, SEXP Times, SEXP Func, SEXP Initfunc,
   SEXP  R_yout;
   double *y0,  *y1,  *y2,  *dy1,  *dy2, *out, *yout;
 
-  double err=0, errold=0, dtnew=0, t, dt, t_ext, tmax;
+  double t, dt, tmax;
 
   SEXP R_FSAL, Alpha, Beta;
   int fsal = FALSE;       /* assume no FSAL */
   int interpolate = TRUE; /* polynomial interpolation is done by default */
 
-  int i = 0, j=0, j1=0, k, it=0, it_tot=0, it_ext=0, nt = 0, neq=0;
-  int accept = 0;
-  int one=1, isForcing;
+  int i = 0, j=0, it=0, it_tot = 0, it_ext = 0, nt = 0, neq = 0;
+  int isForcing;
 
   /*------------------------------------------------------------------------*/
   /* Processing of Arguments                                                */
@@ -190,7 +191,7 @@ SEXP call_rkAuto(SEXP Xstart, SEXP Times, SEXP Func, SEXP Initfunc,
   iknots++;
 
   t = tt[0];
-  tmax = fmax(tt[nt], tcrit);
+  tmax = fmax(tt[nt], tcrit); // fixme! ?? fmin??
   dt = fmin(hmax, hini);
   hmax = fmin(hmax, tmax - t);
 
@@ -210,165 +211,84 @@ SEXP call_rkAuto(SEXP Xstart, SEXP Times, SEXP Func, SEXP Initfunc,
   it     = 1; /* step counter; zero element is initial state   */
   it_ext = 0; /* counter for external time step (dense output) */
   it_tot = 0; /* total number of time steps                    */
+  
+  // new code !!!
 
-  do { 
-    /******  save former results of last step if the method allows this
-            (first same as last)                                       ******/
-    if (fsal && accept){
-      j1 = 1;
-      for (i = 0; i < neq; i++) FF[i] = FF[i + neq * (stage - 1)];
-    } else {
-      j1 = 0;
+  if (interpolate) {
+  // the ordinary way
+    rk_auto(
+         // int
+         fsal,  neq,  stage,
+         isDll,  isForcing,  verbose,
+         nknots,  interpolate, maxsteps, 
+  	     nt,
+  	     // int*
+  	     &iknots, &it, &it_ext, &it_tot,
+         //  arrays
+         istate, ipar,
+  	     // double
+  	     t,  tmax, hmin, hmax,  alpha,  beta,
+  	     // double*
+  	     &dt,
+         // double arrays
+  	     tt,
+         y0,  y1,  y2,  dy1,  dy2,
+         f,  y,  Fj,  tmp, 
+         FF,  rr,
+  	     A,
+  	     out,  bb1,  bb2,  cc,  dd,
+  	     atol,  rtol,
+  	     yknots,  yout,
+  	     // SEXPs
+  	     Func, Parms, Rho
+    );
+  } else {
+     // one call per time stept (or event)
+     for (int j = 0; j < nt - 1; j++) {
+       /* 
+         ToDo:
+           - respect stepping of events (if available)
+             ? with or without combined interpolation?? --> without
+           - re-use last step size as hmin   --> solved
+       
+       */
+     
+       // Rprintf("\nNumber of time steps it = %d, it_ext = %d, it_tot = %d\n", 
+       //  it, it_ext, it_tot);
+       t = tt[j];
+       //tmax = fmax(tt[j + 1], tcrit); // handle fcrit correctly !!! fmin??
+       tmax = fmin(tt[j + 1], tcrit);
+       //Rprintf("\n %d th time interval = %g ... %g", j, t, tmax);
+       //Rprintf("\ndt=%g", dt);
+       rk_auto(
+           // int
+           fsal,  neq,  stage,
+           isDll,  isForcing,  verbose,
+           nknots,  interpolate, maxsteps, 
+    	     nt,
+    	     // int*
+    	     &iknots, &it, &it_ext, &it_tot,
+           //  arrays
+           istate, ipar,
+    	     // double
+    	     t,  tmax, hmin, hmax,  alpha,  beta,
+    	     // double*
+  	       &dt,
+           // double arrays
+    	     tt,
+           y0,  y1,  y2,  dy1,  dy2,
+           f,  y,  Fj,  tmp, 
+           FF,  rr,
+    	     A,
+    	     out,  bb1,  bb2,  cc,  dd,
+    	     atol,  rtol,
+    	     yknots,  yout,
+    	     // SEXPs
+    	     Func, Parms, Rho
+      );
     }
-    /******  Prepare Coefficients from Butcher table ******/
-    for (j = j1; j < stage; j++) {
-      for(i = 0; i < neq; i++) Fj[i] = 0;
-        k = 0;
-        while(k < j) {
-          for(i = 0; i < neq; i++)
-            Fj[i] = Fj[i] + A[j + stage * k] * FF[i + neq * k] * dt;
-          k++;
-        }
-        for (int i = 0; i < neq; i++) {
-          tmp[i] = Fj[i] + y0[i];
-        }
-        /******  Compute Derivatives ******/
-        /* pass option to avoid unnecessary copying in derivs */
-        derivs(Func, t + dt * cc[j], tmp, Parms, Rho, FF, out, j, neq, 
-               ipar, isDll, isForcing);
-    }
+  }
 
-    /*====================================================================*/
-    /* Estimation of new values                                           */
-    /*====================================================================*/
-
-    /* use BLAS wrapper with reduced error checking */
-    blas_matprod1(FF, neq, stage, bb1, stage, one, dy1);
-    blas_matprod1(FF, neq, stage, bb2, stage, one, dy2);
-
-    it_tot++; /* count total number of time steps */
-    for (i = 0; i < neq; i++) {
-      y1[i] = y0[i] +  dt * dy1[i];
-      y2[i] = y0[i] +  dt * dy2[i];
-    }
-
-    /*====================================================================*/
-    /*      stepsize adjustment                                           */
-    /*====================================================================*/
-    
-    err = maxerr(y1, y2, atol, rtol, neq);
-    dtnew = dt;
-    //accept = TRUE;
-    if (err == 0) {  /* use max scale if all tolerances are zero */
-      dtnew = fmin(dt * 10, hmax);
-      errold = fmax(err, 1e-4); // 1e-4 taken from Press et al.
-      accept = TRUE;
-    } else if (err < 1.0) {
-      //dtnew = fmin(hmax, dt * 0.9 * pow(err, -1.0/qerr));
-      // increase step size only if last one was accepted
-      if (accept) dtnew = fmin(hmax, dt * 0.9 * pow(err, -alpha) * pow(errold, beta));
-      errold = fmax(err, 1e-4); // 1e-4 taken from Press et al.
-      accept = TRUE;
-    } else if (err > 1.0) {
-      accept = FALSE;
-      //dtnew = dt * fmax(0.9 * pow(err, -1.0/qerr), 0.2);
-      dtnew = dt * fmax(0.9 * pow(err, -alpha), 0.2);
-    }
-
-    if (dtnew < hmin) {
-      accept=TRUE;
-      if (verbose) Rprintf("warning, h < Hmin\n"); // remove this later ...
-      istate[0] = -2;
-      dtnew = hmin;
-    }
-    /*====================================================================*/
-    /*      Interpolation and Data Storage                                */
-    /*====================================================================*/
-    if (accept) {
-      if (interpolate) {
-      /*--------------------------------------------------------------------*/
-      /* case A) "Dense Output": built-in polynomial interpolation          */
-      /* available for certain rk formulae, e.g. for rk45dp7                */
-      /*--------------------------------------------------------------------*/
-	if (dd) { /* i.e. if dd is not Zero */
-        denspar(FF, y0, y1, dt, dd, neq, stage, rr);
-        t_ext = tt[it_ext];
-        while (t_ext <= t + dt) {
-          densout(rr, t, t_ext, dt, tmp, neq);
-          /* store outputs */
-          if (it_ext < nt) {
-            yout[it_ext] = t_ext;
-            for (i = 0; i < neq; i++)
-              yout[it_ext + nt * (1 + i)] = tmp[i];
-          }
-          if(it_ext < nt) t_ext = tt[++it_ext]; else break;
-        }
-        /*--------------------------------------------------------------------*/
-        /* case B) "Neville-Aitken-Interpolation" for integrators             */
-        /* without dense output                                               */
-        /*--------------------------------------------------------------------*/
-        } else {
-          /* (1) collect number "nknots" of knots in advanve */
-          yknots[iknots] = t + dt;   /* time is first column */
-          for (i = 0; i < neq; i++) yknots[iknots + nknots * (1 + i)] = y2[i];
-          if (iknots < (nknots - 1)) {
-            iknots++;
-          } else {
-	   /* (2) do polynomial interpolation */
-           t_ext = tt[it_ext];
-           while (t_ext <= t + dt) {
-            neville(yknots, &yknots[nknots], t_ext, tmp, nknots, neq);
-            /* (3) store outputs */
-            if (it_ext < nt) {
-              yout[it_ext] = t_ext;
-              for (i = 0; i < neq; i++)
-                yout[it_ext + nt * (1 + i)] = tmp[i];
-            }
-            if(it_ext < nt) t_ext = tt[++it_ext]; else break;
-           }
-           shiftBuffer(yknots, nknots, neq + 1);
-          }
-        }
-      } else {
-        /*--------------------------------------------------------------------*/
-        /* Case C) no interpolation at all (for testing purposes)             */
-        /* Note that this works only if time steps match exactly              */
-        /* i.e. not in all cases because of floating point inaccuracy         */
-        /* but that's the price for "no interpolation"                        */
-        /*--------------------------------------------------------------------*/
-        t_ext = tt[it_ext];
-        while (t_ext <= t + dt) {
-          if (it_ext < nt) {
-            yout[it_ext] = t_ext;
-            if (it < nt) {
-              /* all time steps */
-              yout[it_ext] = t_ext;
-              /* only matching time steps */
-              if (t_ext == t + dt)
-                for (i = 0; i < neq; i++) yout[it_ext + nt * (1 + i)] = y2[i];
-            }
-          }
-          if(it_ext < nt) t_ext = tt[++it_ext]; else break;
-        }
-      }
-      /*--------------------------------------------------------------------*/
-      /* next time step                                                     */
-      /*--------------------------------------------------------------------*/
-      t = t + dt;
-      it++;
-      for (i=0; i < neq; i++) y0[i] = y2[i];
-    } /* else rejected time step */
-    dt = fmin(dtnew, tmax - t);
-    if (it_ext > nt) {
-      Rprintf("error in rk_solvers.c - call_rkauto: output buffer overflow\n");
-      break;
-    }
-    if (it_tot > maxsteps) {
-      if (verbose) Rprintf("Max. number of steps exceeded\n");
-      istate[0] = -1;
-      break;
-    }
-  } while (t < tmax); /* end of rk main loop */
 
   /*====================================================================*/
   /* call derivs again to get global outputs                            */
@@ -388,7 +308,7 @@ SEXP call_rkAuto(SEXP Xstart, SEXP Times, SEXP Func, SEXP Initfunc,
   setIstate(R_yout, R_istate, istate, it_tot, stage, fsal, qerr);
 
   /* release R resources */
-  if (verbose) 
+  //if (verbose) 
     Rprintf("\nNumber of time steps it = %d, it_ext = %d, it_tot = %d\n", 
       it, it_ext, it_tot);
   unprotect_all();
---
+++ pkg/deSolve/src/rk_auto.h
@@ -0,0 +1,217 @@
+/*==========================================================================*/
+/* Runge-Kutta Solvers, (C) Th. Petzoldt, License: GPL >=2                  */
+/* General RK Solver for methods with adaptive step size                    */
+/* -- main loop == core function --                                                      */
+/*==========================================================================*/
+
+//#include "rk_util.h"
+
+void rk_auto(
+       // int
+	     int fsal, int neq, int stage,
+	     int isDll, int isForcing, int verbose,
+	     int nknots, int interpolate, int maxsteps,
+	     int nt,
+	     // int*
+	     int* iiknots, int* iit, int* iit_ext, int* iit_tot, 
+       // int arrays
+       int* istate,  int* ipar,
+	     // double
+	     double t, double tmax, double hmin, double hmax, 
+       double alpha, double beta,
+       // double*
+       double* ddt, 
+       // double arrays
+	     double* tt,
+       double* y0, double* y1, double* y2, double* dy1, double* dy2,
+       double* f, double* y, double* Fj, double* tmp,
+       double* FF, double* rr,
+	     double* A, 
+	     double* out, double* bb1, double* bb2, double* cc, double* dd, 
+	     double* atol, double* rtol,
+	     double* yknots, double* yout,
+	     // SEXPs
+	     SEXP Func, SEXP Parms, SEXP Rho
+  ) {
+
+  int i = 0, j = 0, j1 = 0, k = 0, accept = 0, one = 1;
+  int iknots = *iiknots, it = *iit, it_ext = *iit_ext, it_tot = *iit_tot;
+  double err, errold = 0, dtnew, t_ext;
+  double dt = *ddt;
+
+  //t = tt[0];
+  //tmax = fmax(tt[nt], tcrit);
+  //dt = fmin(hmax, hini);
+  //hmax = fmin(hmax, tmax - t);
+
+
+  /*------------------------------------------------------------------------*/
+  /* Main Loop                                                              */
+  /*------------------------------------------------------------------------*/
+  //it     = 1; /* step counter; zero element is initial state   */
+  //it_ext = 0; /* counter for external time step (dense output) */
+  //it_tot = 0; /* total number of time steps                    */
+
+  do { 
+    /******  save former results of last step if the method allows this
+            (first same as last)                                       ******/
+    if (fsal && accept){
+      j1 = 1;
+      for (i = 0; i < neq; i++) FF[i] = FF[i + neq * (stage - 1)];
+    } else {
+      j1 = 0;
+    }
+    /******  Prepare Coefficients from Butcher table ******/
+    for (j = j1; j < stage; j++) {
+      for(i = 0; i < neq; i++) Fj[i] = 0;
+        k = 0;
+        while(k < j) {
+          for(i = 0; i < neq; i++)
+            Fj[i] = Fj[i] + A[j + stage * k] * FF[i + neq * k] * dt;
+          k++;
+        }
+        for (int i = 0; i < neq; i++) {
+          tmp[i] = Fj[i] + y0[i];
+        }
+        /******  Compute Derivatives ******/
+        /* pass option to avoid unnecessary copying in derivs */
+        derivs(Func, t + dt * cc[j], tmp, Parms, Rho, FF, out, j, neq, 
+               ipar, isDll, isForcing);
+    }
+
+    /*====================================================================*/
+    /* Estimation of new values                                           */
+    /*====================================================================*/
+
+    /* use BLAS wrapper with reduced error checking */
+    blas_matprod1(FF, neq, stage, bb1, stage, one, dy1);
+    blas_matprod1(FF, neq, stage, bb2, stage, one, dy2);
+
+    it_tot++; /* count total number of time steps */
+    for (i = 0; i < neq; i++) {
+      y1[i] = y0[i] +  dt * dy1[i];
+      y2[i] = y0[i] +  dt * dy2[i];
+    }
+
+    /*====================================================================*/
+    /*      stepsize adjustment                                           */
+    /*====================================================================*/
+    
+    err = maxerr(y1, y2, atol, rtol, neq);
+    dtnew = dt;
+    //accept = TRUE;
+    if (err == 0) {  /* use max scale if all tolerances are zero */
+      dtnew = fmin(dt * 10, hmax);
+      errold = fmax(err, 1e-4); // 1e-4 taken from Press et al.
+      accept = TRUE;
+    } else if (err < 1.0) {
+      //dtnew = fmin(hmax, dt * 0.9 * pow(err, -1.0/qerr));
+      // increase step size only if last one was accepted
+      if (accept) dtnew = fmin(hmax, dt * 0.9 * pow(err, -alpha) * pow(errold, beta));
+      errold = fmax(err, 1e-4); // 1e-4 taken from Press et al.
+      accept = TRUE;
+    } else if (err > 1.0) {
+      accept = FALSE;
+      //dtnew = dt * fmax(0.9 * pow(err, -1.0/qerr), 0.2);
+      dtnew = dt * fmax(0.9 * pow(err, -alpha), 0.2);
+    }
+
+    if (dtnew < hmin) {
+      accept=TRUE;
+      if (verbose) Rprintf("warning, h < Hmin\n"); // remove this later ...
+      istate[0] = -2;
+      dtnew = hmin;
+    }
+    /*====================================================================*/
+    /*      Interpolation and Data Storage                                */
+    /*====================================================================*/
+    if (accept) {
+      if (interpolate) {
+      /*--------------------------------------------------------------------*/
+      /* case A) "Dense Output": built-in polynomial interpolation          */
+      /* available for certain rk formulae, e.g. for rk45dp7                */
+      /*--------------------------------------------------------------------*/
+	if (dd) { /* i.e. if dd is not Zero */
+        denspar(FF, y0, y1, dt, dd, neq, stage, rr);
+        t_ext = tt[it_ext];
+        while (t_ext <= t + dt) {
+          densout(rr, t, t_ext, dt, tmp, neq);
+          /* store outputs */
+          if (it_ext < nt) {
+            yout[it_ext] = t_ext;
+            for (i = 0; i < neq; i++)
+              yout[it_ext + nt * (1 + i)] = tmp[i];
+          }
+          if(it_ext < nt) t_ext = tt[++it_ext]; else break;
+        }
+        /*--------------------------------------------------------------------*/
+        /* case B) "Neville-Aitken-Interpolation" for integrators             */
+        /* without dense output                                               */
+        /*--------------------------------------------------------------------*/
+        } else {
+          /* (1) collect number "nknots" of knots in advanve */
+          yknots[iknots] = t + dt;   /* time is first column */
+          for (i = 0; i < neq; i++) yknots[iknots + nknots * (1 + i)] = y2[i];
+          if (iknots < (nknots - 1)) {
+            iknots++;
+          } else {
+	   /* (2) do polynomial interpolation */
+           t_ext = tt[it_ext];
+           while (t_ext <= t + dt) {
+            neville(yknots, &yknots[nknots], t_ext, tmp, nknots, neq);
+            /* (3) store outputs */
+            if (it_ext < nt) {
+              yout[it_ext] = t_ext;
+              for (i = 0; i < neq; i++)
+                yout[it_ext + nt * (1 + i)] = tmp[i];
+            }
+            if(it_ext < nt) t_ext = tt[++it_ext]; else break;
+           }
+           shiftBuffer(yknots, nknots, neq + 1);
+          }
+        }
+      } else {
+        /*--------------------------------------------------------------------*/
+        /* Case C) no interpolation at all (for testing purposes)             */
+        /* Note that this works only if time steps match exactly              */
+        /* i.e. not in all cases because of floating point inaccuracy         */
+        /* but that's the price for "no interpolation"                        */
+        /*--------------------------------------------------------------------*/
+        t_ext = tt[it_ext];
+        while (t_ext <= t + dt) {
+          if (it_ext < nt) {
+            yout[it_ext] = t_ext;
+            if (it < nt) {
+              /* all time steps */
+              yout[it_ext] = t_ext;
+              /* only matching time steps */
+              if (t_ext == t + dt)
+                for (i = 0; i < neq; i++) yout[it_ext + nt * (1 + i)] = y2[i];
+            }
+          }
+          if(it_ext < nt) t_ext = tt[++it_ext]; else break;
+        }
+      }
+      /*--------------------------------------------------------------------*/
+      /* next time step                                                     */
+      /*--------------------------------------------------------------------*/
+      t = t + dt;
+      it++;
+      for (i=0; i < neq; i++) y0[i] = y2[i];
+    } /* else rejected time step */
+    dt = fmin(dtnew, tmax - t);
+    if (it_ext > nt) {
+      Rprintf("error in rk_solvers.c - call_rkauto: output buffer overflow\n");
+      break;
+    }
+    if (it_tot > maxsteps) {
+      if (verbose) Rprintf("Max. number of steps exceeded\n");
+      istate[0] = -1;
+      break;
+    }
+  } while (t < tmax); /* end of rk main loop */
+  
+  // return reference values the simple way, fix this later
+  *iiknots = iknots; *iit = it; *iit_ext = it_ext; *iit_tot = it_tot;
+  *ddt = dtnew;
+}
