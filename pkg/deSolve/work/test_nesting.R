be2f2aa63fe7ce508d02c431dd67b926a6a7f7d1
petzoldt <petzoldt@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-03-31 10:45:40 +0000

    test_nesting.R

--- pkg/deSolve/work/test_nesting.R
+++ pkg/deSolve/work/test_nesting.R
@@ -9,15 +9,14 @@ func1 <- function(t,y,p) {
     list(-0.1*y)
    }
    #AA <- ode(y=5,times=c(0.15, 0.4), p=NULL, func=ff, method="euler")
-   AA <- ode(y=5,times=c(0.15, 0.4, 0.77), p=NULL,
-           func=ff, method="lsoda") # stack imbalance !!!
-   #AA <- euler(y=5,times=sort(sample(1:10, 3)/10), p=NULL, func=ff) # special "euler"
+   AA <- ode(y=5,times=c(0.15, 0.4, 0.77), p=NULL, func=ff, method="lsoda")
 
   list(-0.1*y)
 }
 out <- rk(y=8,times=sort(sample(1:20, 5)), p=NULL,func=func1, method="euler")
 ## can be called repeatedly without problems
 
-out <- ode(y=8, times=sort(sample(1:20, 5)), p=NULL,func=func1)
+out <- ode(y=8, times=sort(sample(1:20, 5)), p=NULL,func=func1, hmax=1)
+## ---> does not recognize time steps (e.g. hmax)
 ## ---> crashes R after repeated calls
 
