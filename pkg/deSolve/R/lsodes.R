070192ab7a5eb8067e7e31d9335556c285991e71
karlines <karlines@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-07-12 09:46:42 +0000

    liw

--- pkg/deSolve/R/lsodes.R
+++ pkg/deSolve/R/lsodes.R
@@ -260,10 +260,11 @@ lsodes <- function(y, times, func, parms, rtol = 1e-6, atol = 1e-6,
     if (sparsetype == "1D") lrw <- lrw*1.2 # increase to be sure it is enough...
   }
 
-  if (is.null(liw)) {         # make a guess of integer work space needed
-    if (moss == 0 && miter %in% c(1,2)) liw <- 31+n+nnz +30 else  # extra 30
-                                        liw <- 30
-  }
+#  if (is.null(liw)) {         # make a guess of integer work space needed
+    if (moss == 0 && miter %in% c(1,2)) liw <- max(liw, 31+n+nnz +30) else  # extra 30
+                                        liw <- max(liw, 30)
+#  }
+  
   lrw <- lrw + 3*nroot
   # 2. Allocate and set values
   # only first 20 elements of rwork passed to solver;
