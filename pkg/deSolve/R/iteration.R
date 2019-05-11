1bfab8d25a9520611ebb037370ebb677047fe8a1
karlines <karlines@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-05-11 06:07:35 +0000

    attribute type set before diagnostics (failed when verbose = TRUE)

--- pkg/deSolve/R/iteration.R
+++ pkg/deSolve/R/iteration.R
@@ -1,6 +1,6 @@
 ### ============================================================================
 ### Interface to C code for Euler's ODE solver
-### with fixed step size and without interpolation, see helpfile for details.
+### with fixed step size and without integration, see helpfile for details.
 ### ============================================================================
 
 iteration <- function(y, times, func, parms, hini = NULL,
@@ -88,8 +88,9 @@ iteration <- function(y, times, func, parms, hini = NULL,
     ## saving results
     out <- saveOutrk(out, y, n, Nglobal, Nmtot,
                      iin = c(1, 12, 13, 15), iout = c(1:3, 18))
-    if (verbose) diagnostics(out)
+                     
     attr(out, "type") <- "iteration"
+    if (verbose) diagnostics(out)
     out
 }
 
