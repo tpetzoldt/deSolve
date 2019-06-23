acba12d8907a6a996df35bcca46b6f86796c488b
petzoldt <petzoldt@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-06-23 17:50:56 +0000

    remove some obsolete fortran print functions (three such functions are still used)

--- pkg/deSolve/src/errmsg.f
+++ pkg/deSolve/src/errmsg.f
@@ -1,16 +1,3 @@
-      subroutine rprint(msg)
-      character (len=*) msg
-        call dblepr(msg, -1, 0, 0)
-      end subroutine 
-
-      subroutine rprintid(msg, i1, d1)
-      character (len=*) msg
-      double precision d1
-      integer i1
-        call dblepr(msg, -1, d1, 1)
-        call intpr(" ", -1, i1, 1)
-      end subroutine 
-
       subroutine rprintd1(msg, d1)
       character (len=*) msg
       double precision d1
@@ -29,29 +16,6 @@
       character (len=*) msg
       integer i1
         call intpr(msg, -1, i1, 1)
-      end subroutine 
-
-      subroutine rprinti2(msg, i1, i2)
-      character (len=*) msg
-      INTEGER IN(2), i1, i2
-        IN(1) = i1
-        IN(2) = i2
-        call intpr(msg, -1, IN, 2)
-      end subroutine 
-
-      subroutine rprinti3(msg, i1, i2, i3)
-      character (len=*) msg
-      INTEGER IN(3), i1, i2, i3
-        IN(1) = i1
-        IN(2) = i2
-        IN(3) = i3
-        call intpr(msg, -1, IN, 3)
-      end subroutine 
-
-      subroutine rprint2(msg)
-      implicit none
-      character (len = *) msg
-        call dblepr(msg, 61, 0, 0)
       end subroutine 
 
 
